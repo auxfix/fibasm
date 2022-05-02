b455a1695f9fdf9cbcc7d6c0537225a1ee9b3aa5
aux_fix <music.wave.form7@gmail.com>
2022-05-02 06:58:21 +0200

    add hello message
    first syscalls

--- fib.s
+++ fib.s
@@ -1,12 +1,23 @@
 global  _start
 
+section .data
+    message db "Fibonacci Sequence:", 0x0a
+
 section .text
 _start:
-    xor rbx, rbx
-    xor rax, rax
-    inc rbx,
+    mov rax, 1       ; rax: syscall number 1
+    mov rdi, 1      ; rdi: fd 1 for stdout
+    mov rsi,message ; rsi: pointer to message
+    mov rdx, 20      ; rdx: print length of 20 bytes
+    syscall         ; call write syscall to the intro message
+    xor rax, rax    ; initialize rax to 0
+    xor rbx, rbx    ; initialize rbx to 0
+    inc rbx         ; increment rbx to 1
 loopFib:
-    add rax, rbx
-    xchg rax, rbx
-    cmp rbx, 10
-    js loopFib		
+    add rax, rbx    ; get the next number
+    xchg rax, rbx   ; swap values
+    cmp rbx, 10		; do rbx - 10
+    js loopFib		; jump if result is <0
+    mov rax, 60
+    mov rdi, 0
+    syscall	
