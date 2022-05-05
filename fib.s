961bb712d019de33139fc8e875f5bc6d1728ca3f
aux_fix <music.wave.form7@gmail.com>
2022-05-05 04:57:40 +0200

    print fib numbers
    inline, till 8

--- fib.s
+++ fib.s
@@ -2,8 +2,28 @@ global  _start
 
 section .data
     message db "Fibonacci Sequence:", 0x0a
+section .bss
+    num    resd 1
+    buffer resb 10
 
 section .text
+int_to_string:
+  add esi,9
+  mov byte [esi],0    ; String terminator
+
+  mov ebx,10
+.next_digit:
+  xor edx,edx         ; Clear edx prior to dividing edx:eax by ebx
+  div ebx             ; eax /= 10
+  add dl,'0'          ; Convert the remainder to ASCII 
+  dec esi             ; store characters in reverse order
+  mov [esi],dl
+  test eax,eax            
+  jnz .next_digit     ; Repeat until eax==0
+
+  ; return a pointer to the first digit (not necessarily the start of the provided buffer)
+  mov eax,esi
+  ret
 _start:
     mov rax, 1       ; rax: syscall number 1
     mov rdi, 1      ; rdi: fd 1 for stdout
@@ -16,6 +36,19 @@ _start:
 loopFib:
     add rax, rbx    ; get the next number
     xchg rax, rbx   ; swap values
+    push rax
+    push rbx	
+    ; print number
+    mov    [num],rax
+    
+    mov    eax,[num]       ; function args using our own private calling convention
+    mov    esi,buffer
+    call   int_to_string		
+    mov rax, 1
+    mov rdi, 1      ; print fib number 
+    syscall
+    pop rbx
+    pop rax          ;	
     cmp rbx, 10		; do rbx - 10
     js loopFib		; jump if result is <0
     mov rax, 60
