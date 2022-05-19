88e8af21541685d85fe7c1001c08d04a903c8062
aux_fix <music.wave.form7@gmail.com>
2022-05-19 07:13:27 +0200

    change print function one library one
    use stadart printf

--- assembler.sh
+++ assembler.sh
@@ -3,5 +3,5 @@
 fileName="${1%%.*}" # remove .s extension
 
 nasm -f elf64 ${fileName}".s"
-ld ${fileName}".o" -o ${fileName}
+ld ${fileName}".o" -o ${fileName} -lc --dynamic-linker /lib64/ld-linux-x86-64.so.2
 [ "$2" == "-g" ] && gdb -q ${fileName} || ./${fileName}
--- fib.s
+++ fib.s
@@ -1,74 +1,54 @@
 global  _start
+extern  printf
 
 section .data
     message db "Fibonacci Sequence:", 0x0a
-    nline db 0x0a
+    outFormat db  "%d", 0x0a, 0x00
+
 section .bss
     num    resd 1
     buffer resb 10
 
 section .text
+
 _start:
+  call printHello
+  call initFirstFibVal
+  call loopFib
+  call exitSystemCall
 
-call printHello
-call initFirstFibVal
-call loopFib
-call exitSystemCall
 
-int_to_string:
-  add esi,9
-  mov ebx,10
-.next_digit:
-  xor edx,edx         ; Clear edx prior to dividing edx:eax by ebx
-  div ebx             ; eax /= 10
-  add dl,'0'         ; Convert the remainder to ASCII 
-  dec esi             ; store characters in reverse order
-  mov [esi],dl
-  test eax,eax            
-  jnz .next_digit     ; Repeat until eax==0
+printFib:
+    push rax            ; push registers to stack
+    push rbx
+    mov rdi, outFormat  ; set 1st argument (Print Format)
+    mov rsi, rbx        ; set 2nd argument (Fib Number)
+    call printf         ; printf(outFormat, rbx)
+    pop rbx             ; restore registers from stack
+    pop rax
+    ret
 
-  ; return a pointer to the first digit (not necessarily the start of the provided buffer)
-  mov eax,esi
-  ret
 printHello:
     mov rax, 1       ; rax: syscall number 1
     mov rdi, 1      ; rdi: fd 1 for stdout
     mov rsi,message ; rsi: pointer to message
     mov rdx, 20      ; rdx: print length of 20 bytes
-    syscall         ; call write syscall to the intro message
-    
-printNewLine:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, nline
-    mov rdx, 1
-    syscall
-    ret		 	
+    syscall         ; call write syscall to the intro message	 	
+
 initFirstFibVal:    
     xor rax, rax    ; initialize rax to 0
     xor rbx, rbx    ; initialize rbx to 0
     inc rbx         ; increment rbx to 1
     ret
+
 loopFib:
+    call printFib 
     add rax, rbx    ; get the next number
-    xchg rax, rbx   ; swap values
-    push rax
-    push rbx	
-    ; print number
-    mov    [num],rax
-    
-    mov    eax,[num]       ; function args using our own private calling convention
-    mov    esi,buffer
-    call   int_to_string		
-    mov rax, 1
-    mov rdi, 1      ; print fib number 
-    syscall
-    call printNewLine	
-    pop rbx
-    pop rax          ;	
+    xchg rax, rbx   ; swap values	
     cmp rbx, 10		; do rbx - 10
     js loopFib		; jump if result is <0
     ret	   
+
 exitSystemCall:
     mov rax, 60
     mov rdi, 0
